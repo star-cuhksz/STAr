26d0621e166998aea922743bf3a55cfdf1fdce1d
Brando Zhang <116010308@link.cuhk.edu.cn>
2018-06-06 23:00:30 +0800

    Add some comments on lk_track.py
    Improve readability

--- develop/Detector/lk_track.py
+++ develop/Detector/lk_track.py
@@ -27,10 +27,12 @@ import video
 from common import anorm2, draw_str
 from time import clock
 
+# Parameters for lucas kanade optical flow
 lk_params = dict( winSize  = (15, 15),
                   maxLevel = 2,
                   criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
+# Parameters for corner detection
 feature_params = dict( maxCorners = 500,
                        qualityLevel = 0.3,
                        minDistance = 7,
@@ -38,15 +40,39 @@ feature_params = dict( maxCorners = 500,
 
 class App:
     def __init__(self, video_src):
+        """
+        Optical flow initiation
+        :param  video_src: the path of video source
+        :datafield  self.track_len:
+                    self.detect_interval: frequency of finding feature points
+                    self.tracks: tracking points
+                    self.cam: video instance obtained from video_src
+                    self.frame_idx:
+                    self.trace_color: color of the trace, BGR pattern
+        """
         self.track_len = 10
         self.detect_interval = 5
         self.tracks = []
         self.cam = video.create_capture(video_src)
         self.frame_idx = 0
+        self.trace_color = (0, 0, 255)
+
+    # def get_velocity(self, point_prev, point_curr):
+    #     """
+    #     Calculate the velocity of the interested feature points.
+    #     :param point_prev: previous point in type numpy.array
+    #     :param point_curr: current point in type numpy.array
+    #     :return: the velocity of this point in unit 'pixel/frame'
+    #     """
+    #     distance = np.linalg.norm(point_prev - point_curr)
+    #     velocity = float(distance / self.detect_interval)
+    #     return velocity
 
     def run(self):
         while True:
             _ret, frame = self.cam.read()
+
+            # avoid crashing if frame not found
             if _ret:
                 frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
             else:
@@ -68,29 +94,35 @@ class App:
                     if len(tr) > self.track_len:
                         del tr[0]
                     new_tracks.append(tr)
-                    cv.circle(vis, (x, y), 2, (0, 255, 0), -1)
+                    cv.circle(img=vis, center=(x, y), radius=2, color=self.trace_color, thickness=-1)
                 self.tracks = new_tracks
                 cv.polylines(vis, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
                 draw_str(vis, (20, 20), 'track count: %d' % len(self.tracks))
 
+            # detect feature points every detect_interval frames
             if self.frame_idx % self.detect_interval == 0:
+                # initiate the mask
                 mask = np.zeros_like(frame_gray)
                 mask[:] = 255
                 for x, y in [np.int32(tr[-1]) for tr in self.tracks]:
-                    cv.circle(mask, (x, y), 5, 0, -1)
-                p = cv.goodFeaturesToTrack(frame_gray, mask = mask, **feature_params)
+                    cv.circle(img=mask, center=(x, y), radius=5, color=0, thickness=-1)
+                p = cv.goodFeaturesToTrack(frame_gray, mask=mask, **feature_params)
                 if p is not None:
                     for x, y in np.float32(p).reshape(-1, 2):
                         self.tracks.append([(x, y)])
 
-
+            # update previous frame
             self.frame_idx += 1
             self.prev_gray = frame_gray
             cv.imshow('lk_track', vis)
 
+            # press ESC to quit
             ch = cv.waitKey(1)
             if ch == 27:
                 break
+
+        # release the camera if used
+        self.cam.release()
 
 def main():
     import sys
