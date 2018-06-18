defebec2b312e1640a4ca55abea4ec76eea7dddf
Brando Zhang <116010308@link.cuhk.edu.cn>
2018-06-18 20:56:47 +0800

    Format: pid_test.ino.ino
    Format the code for readability.

--- develop/Autonomous Control/pid_test.ino/pid_test.ino.ino
+++ develop/Autonomous Control/pid_test.ino/pid_test.ino.ino
@@ -8,38 +8,38 @@
  * when we're near setpoint and more agressive Tuning
  * Parameters when we're farther away.
  ********************************************************/
-int comInt;  
+int comInt;
 #include <Servo.h>
 #include <PID_v1.h>
 #include <JY901.h>
 #include <Adafruit_INA219.h>
 //
 
-
 //float shuntvoltage = 0;
 //float busvoltage = 0;
-//float current_mA = 0; 
+//float current_mA = 0;
 //float loadvoltage = 0;
 //these variables are for current sensor
 
-
-Servo rudder;  // create servo object to control a servo
+Servo rudder; // create servo object to control a servo
 Servo sail;
-int pos_rudder=90;
-int pos_sail=115;
-long time,lasttime;
+int pos_rudder = 90;
+int pos_sail = 115;
+long time, lasttime;
 //Define Variables we'll be connecting to
-double Setpoint, Input, Output,ini_Input;
-int Ms=200;
+double Setpoint, Input, Output, ini_Input;
+int Ms = 200;
 //Define the aggressive and conservative Tuning Parameters
-double aggKp=4, aggKi=0.2, aggKd=1;
-double consKp=1, consKi=0.05, consKd=0.25;
+double aggKp = 4, aggKi = 0.2, aggKd = 1;
+double consKp = 1, consKi = 0.05, consKd = 0.25;
 
 //Specify the links and initial tuning parameters
 PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
-void move_rudder(){
-  if(45<=pos_rudder && pos_rudder<=123){
+void move_rudder()
+{
+  if (45 <= pos_rudder && pos_rudder <= 123)
+  {
     rudder.write(pos_rudder);
     delay(10);
   }
@@ -49,11 +49,13 @@ void setup()
   rudder.attach(4);
   sail.attach(5);
   Serial.begin(57600);
-  JY901.StartIIC();  
-  while(Serial.read()>= 0){}//clear serialbuffer  
-  
+  JY901.StartIIC();
+  while (Serial.read() >= 0)
+  {
+  } //clear serialbuffer
+
   //initialize the variables we're linked to
-  lasttime=millis();
+  lasttime = millis();
   Setpoint = 55;
 
   //turn the PID on
@@ -63,39 +65,46 @@ void setup()
 
 void loop()
 {
-  char command=Serial.read();
-  if(command==byte(',')){   
-      Setpoint = Serial.parseInt();
+  char command = Serial.read();
+  if (command == byte(','))
+  {
+    Setpoint = Serial.parseInt();
   }
   JY901.GetAngle();
-  ini_Input = (float)JY901.stcAngle.Angle[2]/32768*180;
-  double gap = abs(Setpoint-ini_Input); //distance away from setpoint
+  ini_Input = (float)JY901.stcAngle.Angle[2] / 32768 * 180;
+  double gap = abs(Setpoint - ini_Input); //distance away from setpoint
   if (gap < 50)
-  {  //we're close to setpoint, use conservative tuning parameters
+  { //we're close to setpoint, use conservative tuning parameters
     myPID.SetTunings(consKp, consKi, consKd);
   }
   else
   {
-     //we're far from setpoint, use aggressive tuning parameters
-     myPID.SetTunings(aggKp, aggKi, aggKd);
+    //we're far from setpoint, use aggressive tuning parameters
+    myPID.SetTunings(aggKp, aggKi, aggKd);
   }
   // To garentee the boat to turn correctly in the following two situation
-  if(Setpoint>0 && ini_Input<Setpoint-180){
-    Input=360+ini_Input;
+  if (Setpoint > 0 && ini_Input < Setpoint - 180)
+  {
+    Input = 360 + ini_Input;
   }
-  else if(Setpoint<0 && ini_Input>Setpoint+180){
-    Input=ini_Input-360;
+  else if (Setpoint < 0 && ini_Input > Setpoint + 180)
+  {
+    Input = ini_Input - 360;
   }
-  else{
-    Input=ini_Input;
+  else
+  {
+    Input = ini_Input;
   }
-  myPID.Compute();//the output value will be rewrite here
-  pos_rudder=86+Output*36/255;
+  myPID.Compute(); //the output value will be rewrite here
+  pos_rudder = 86 + Output * 36 / 255;
   move_rudder();
-  time=millis();//去现在时间(ms)
-  if(time-lasttime>=Ms){
-    lasttime=time;
-    Serial.print(ini_Input);Serial.print('b');Serial.print(pos_rudder);Serial.println(Output);
+  time = millis(); //去现在时间(ms)
+  if (time - lasttime >= Ms)
+  {
+    lasttime = time;
+    Serial.print(ini_Input);
+    Serial.print('b');
+    Serial.print(pos_rudder);
+    Serial.println(Output);
   }
-
 }
