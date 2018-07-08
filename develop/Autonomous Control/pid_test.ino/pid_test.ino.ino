1d48f9e3c52e3b02d8915f8dd177053e794b1c46
MaDaO009 <40014763+MaDaO009@users.noreply.github.com>
2018-07-08 18:58:32 +0800

    add:change directions wirelessly

--- develop/Autonomous Control/pid_test.ino/pid_test.ino.ino
+++ develop/Autonomous Control/pid_test.ino/pid_test.ino.ino
@@ -8,103 +8,139 @@
  * when we're near setpoint and more agressive Tuning
  * Parameters when we're farther away.
  ********************************************************/
-int comInt;
+int comInt;  
 #include <Servo.h>
 #include <PID_v1.h>
 #include <JY901.h>
 #include <Adafruit_INA219.h>
 //
 
+
 //float shuntvoltage = 0;
 //float busvoltage = 0;
-//float current_mA = 0;
+//float current_mA = 0; 
 //float loadvoltage = 0;
 //these variables are for current sensor
 
-Servo rudder; // create servo object to control a servo
-Servo sail;
-int pos_rudder = 90;
-int pos_sail = 115;
-long time, lasttime;
-// Define Variables we'll connect to
-double Setpoint, Input, Output, ini_Input;
-int Ms = 200;
-// Define the aggressive and conservative Tuning Parameters
-double aggKp = 4, aggKi = 0.2, aggKd = 1;
-double consKp = 1, consKi = 0.05, consKd = 0.25;
 
-// Specify the links and initial tuning parameters
+Servo rudder;  // create servo object to control a servo
+Servo sail;
+int pos_rudder=90;
+int pos_sail=115;
+long time,lasttime;
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output,ini_Input;
+int Ms=100;
+//Define the aggressive and conservative Tuning Parameters
+//double aggKp=4, aggKi=0.2, aggKd=1;
+//double consKp=1, consKi=0.05, consKd=0.25;
+double aggKp=4, aggKi=0, aggKd=1;
+double consKp=1, consKi=0, consKd=0.25;
+
+//Specify the links and initial tuning parameters
 PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
-void move_rudder()
-{
-  if (45 <= pos_rudder && pos_rudder <= 123)
-  {
+void move_rudder(){
+  if(45<=pos_rudder && pos_rudder<=123){
     rudder.write(pos_rudder);
     delay(10);
   }
 }
+void move_sail(){
+  if(55<=pos_sail && pos_sail<=115){
+    sail.write(pos_sail);
+    
+  }
+}
+
 void setup()
 {
   rudder.attach(4);
   sail.attach(5);
   Serial.begin(57600);
-  JY901.StartIIC();
-  while (Serial.read() >= 0)
-  {
-  } // clear serial buffer
-
-  // initialize the variables we're linked to
-  lasttime = millis();
+  JY901.StartIIC();  
+  while(Serial.read()>= 0){}//clear serialbuffer  
+  
+  //initialize the variables we're linked to
+  lasttime=millis();
   Setpoint = 55;
 
-  // turn the PID on
+  //turn the PID on
   myPID.SetMode(AUTOMATIC);
   myPID.SetOutputLimits(-255, 255);
 }
 
 void loop()
 {
-  char command = Serial.read();
-  if (command == byte(','))
-  {
-    Setpoint = Serial.parseInt();
+//  char command=Serial.read();
+//  if(command==byte(',')){   
+//      Setpoint = Serial.parseInt();
+//  }
+//  JY901.GetAngle();
+//  ini_Input = (float)JY901.stcAngle.Angle[2]/32768*180;
+//  double gap = abs(Setpoint-ini_Input); //distance away from setpoint
+//  if (gap < 50)
+//  {  //we're close to setpoint, use conservative tuning parameters
+//    myPID.SetTunings(consKp, consKi, consKd);
+//  }
+//  else
+//  {
+//     //we're far from setpoint, use aggressive tuning parameters
+//     myPID.SetTunings(aggKp, aggKi, aggKd);
+//  }
+//  // To garentee the boat to turn correctly in the following two situation
+//  if(Setpoint>0 && ini_Input<Setpoint-180){
+//    Input=360+ini_Input;
+//  }
+//  else if(Setpoint<0 && ini_Input>Setpoint+180){
+//    Input=ini_Input-360;
+//  }
+//  else{
+//    Input=ini_Input;
+//  }
+//  myPID.Compute();//the output value will be rewrite here
+//  pos_rudder=86+Output*36/255;
+//  move_rudder();
+  time=millis();//去现在时间(ms)
+  if(time-lasttime>=Ms){
+    char command=Serial.read();
+    if(command==byte(',')){   
+        Setpoint = Serial.parseInt();
+        myPID.SetNewPoint();
+    }
+    if(command==byte('.')){   
+      comInt = Serial.parseInt();     
+      pos_sail=comInt;
+      move_sail();
+    }
+    JY901.GetAngle();
+    ini_Input = (float)JY901.stcAngle.Angle[2]/32768*180;
+    double gap = abs(Setpoint-ini_Input); //distance away from setpoint
+    if (gap < 20)
+    {  //we're close to setpoint, use conservative tuning parameters
+      myPID.SetTunings(consKp, consKi, consKd);
+    }
+    else
+    {
+       //we're far from setpoint, use aggressive tuning parameters
+       myPID.SetTunings(aggKp, aggKi, aggKd);
+    }
+    // To garentee the boat to turn correctly in the following two situation
+    if(Setpoint>0 && ini_Input<Setpoint-180){
+      Input=360+ini_Input;
+    }
+    else if(Setpoint<0 && ini_Input>Setpoint+180){
+      Input=ini_Input-360;
+    }
+    else{
+      Input=ini_Input;
+    }
+    myPID.Compute();//the output value will be rewrite here
+    pos_rudder=84+Output*36/255;
+    move_rudder();
+    lasttime=time;
+    Serial.println(ini_Input);
+//Serial.print('b');Serial.print(pos_rudder);Serial.print('b');Serial.println(Output);
   }
-  JY901.GetAngle();
-  ini_Input = (float)JY901.stcAngle.Angle[2] / 32768 * 180;
-  double gap = abs(Setpoint - ini_Input); // distance away from setpoint
-  if (gap < 50)
-  { // we're close to the setpoint, use conservative tuning parameters
-    myPID.SetTunings(consKp, consKi, consKd);
-  }
-  else
-  {
-    // we're far from the setpoint, use aggressive tuning parameters
-    myPID.SetTunings(aggKp, aggKi, aggKd);
-  }
-  // To garentee the boat to turn correctly in the following two situations
-  if (Setpoint > 0 && ini_Input < Setpoint - 180)
-  {
-    Input = 360 + ini_Input;
-  }
-  else if (Setpoint < 0 && ini_Input > Setpoint + 180)
-  {
-    Input = ini_Input - 360;
-  }
-  else
-  {
-    Input = ini_Input;
-  }
-  myPID.Compute(); //the output value will be rewrite here
-  pos_rudder = 86 + Output * 36 / 255;
-  move_rudder();
-  time = millis(); //去现在时间(ms)
-  if (time - lasttime >= Ms)
-  {
-    lasttime = time;
-    Serial.print(ini_Input);
-    Serial.print('b');
-    Serial.print(pos_rudder);
-    Serial.println(Output);
-  }
+
 }
