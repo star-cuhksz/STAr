baa2c2beb790c8079da473bfaef40fa71a3b5024
MaDaO009 <40014763+MaDaO009@users.noreply.github.com>
2018-12-01 18:05:07 +0800

    Add: programs for testing

---
+++ develop/Autonomous Control/station keeping/real-world testing/simulator.py
@@ -0,0 +1,29 @@
+# from sailboat_for_simulation_pid_v2 import sailboat
+from plot_for_testing import visualazation
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.gridspec as gridspec
+import time
+import math
+import threading
+import random
+import serial
+
+
+
+##visualazation part
+
+
+    
+
+
+
+
+###send and read part
+
+
+my_plot=visualazation()
+
+
+my_plot.plot()
